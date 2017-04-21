a477120e02ccab8978e00e3ec0d9a5c75e33439a
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-04-21 06:05:56 -0300

    Add @Override to Norm#clone

--- src/main/java/npl/Norm.java
+++ src/main/java/npl/Norm.java
@@ -5,24 +5,25 @@ import jason.asSyntax.LogicalFormula;
 
 public class Norm extends AbstractNorm {
 
-	public Norm(String id, Literal consequence, LogicalFormula condition) {
-		if (!consequence.getFunctor().equals(NormativeProgram.FailFunctor)) {
-			if (((Literal) consequence.getTerm(1)).getFunctor().toString().equals(id)) {
-				consequence.setTerm(1, condition);
-			}
-		}
-		this.id = id;
-		this.consequence = consequence;
-		this.condition = condition;
-	}
+    public Norm(String id, Literal consequence, LogicalFormula condition) {
+        if (!consequence.getFunctor().equals(NormativeProgram.FailFunctor)) {
+            if (((Literal) consequence.getTerm(1)).getFunctor().toString().equals(id)) {
+                consequence.setTerm(1, condition);
+            }
+        }
+        this.id = id;
+        this.consequence = consequence;
+        this.condition = condition;
+    }
 
-	public Norm clone() {
-		return new Norm(id, consequence.copy(), (LogicalFormula) condition.clone());
-	}
+    @Override
+    public Norm clone() {
+        return new Norm(id, consequence.copy(), (LogicalFormula) condition.clone());
+    }
 
-	@Override
-	public String toString() {
-		return "norm " + id + ": " + condition + " -> " + consequence;
-	}
+    @Override
+    public String toString() {
+        return "norm " + id + ": " + condition + " -> " + consequence;
+    }
 
 }
