6e0bfa9bd57b10538c36c719d8f7372374aad765
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-04-21 04:53:54 -0300

    Create INorm

---
+++ src/main/java/npl/INorm.java
@@ -0,0 +1,35 @@
+package npl;
+
+import jason.asSyntax.Literal;
+import jason.asSyntax.LogicalFormula;
+
+/**
+ * @author igorcadelima
+ *
+ */
+public interface INorm {
+
+	/**
+	 * Returns the id of the norm which was assigned at creation.
+	 * 
+	 * @return id
+	 */
+	public String getId();
+
+	/**
+	 * Returns formula that determines the activation condition for the norm.
+	 * 
+	 * @return activation condition
+	 */
+	public LogicalFormula getCondition();
+
+	/**
+	 * Returns consequence of the norm activation as a {@link Literal}. It could
+	 * be an obligation, permission, prohibition, or failure.
+	 * 
+	 * @return consequence
+	 * @see DeonticModality
+	 */
+	public Literal getConsequence();
+
+}
