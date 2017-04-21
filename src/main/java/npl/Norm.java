3d04cecc15acbf58e45be9991e2acc3f76341ba2
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-04-21 05:48:53 -0300

    Remove NormFactory
    Signed-off-by: Igor C. A. de Lima <igorcadelima@gmail.com>

--- src/main/java/npl/Norm.java
+++ src/main/java/npl/Norm.java
@@ -5,47 +5,40 @@ import jason.asSyntax.LogicalFormula;
 
 public class Norm {
 
-    // used by the parser
-    public static NormFactory getFactory() {
-        return new NormFactory() {
-            public Norm createNorm(String id, Literal consequence, LogicalFormula activationCondition) {
-                if (!consequence.getFunctor().equals(NormativeProgram.FailFunctor)) {
-                    if ( ((Literal)consequence.getTerm(1)).getFunctor().toString().equals(id)) {
-                        consequence.setTerm(1, activationCondition);
-                    }
-                }
-                return new Norm(id,consequence,activationCondition);
-            }
-        };
-    }    
+	private String id;
+	private Literal consequence;
+	private LogicalFormula condition;
 
-    private String id;
-    private Literal consequence;
-    private LogicalFormula condition;
-    
-    public Norm(String id, Literal head, LogicalFormula body) {
-        this.id = id;
-        this.consequence = head;
-        this.condition = body;
-    }
-    
-    public String getId() {
-        return id;
-    }
-    public Literal getConsequence() {
-        return consequence;
-    }
-    public LogicalFormula getCondition() {
-        return condition;
-    }
-    
-    public Norm clone() {
-        return new Norm(id, consequence.copy(), (LogicalFormula)condition.clone());
-    }
-    
-    @Override
-    public String toString() {
-        return "norm " + id + ": " + condition + " -> " + consequence;
-    }
-    
+	public Norm(String id, Literal consequence, LogicalFormula condition) {
+		if (!consequence.getFunctor().equals(NormativeProgram.FailFunctor)) {
+			if (((Literal) consequence.getTerm(1)).getFunctor().toString().equals(id)) {
+				consequence.setTerm(1, condition);
+			}
+		}
+		this.id = id;
+		this.consequence = consequence;
+		this.condition = condition;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public Literal getConsequence() {
+		return consequence;
+	}
+
+	public LogicalFormula getCondition() {
+		return condition;
+	}
+
+	public Norm clone() {
+		return new Norm(id, consequence.copy(), (LogicalFormula) condition.clone());
+	}
+
+	@Override
+	public String toString() {
+		return "norm " + id + ": " + condition + " -> " + consequence;
+	}
+
 }
--- src/main/java/npl/NormFactory.java
+++
@@ -1,8 +0,0 @@
-package npl;
-
-import jason.asSyntax.Literal;
-import jason.asSyntax.LogicalFormula;
-
-public interface NormFactory {
-    public Norm createNorm(String id, Literal consequence, LogicalFormula activationCondition);
-}
