7d31ba653c6ca45e6fbc6cf3cd9bc499fb0361b3
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-04-21 05:57:13 -0300

    Create AbstractNorm

---
+++ src/main/java/npl/AbstractNorm.java
@@ -0,0 +1,26 @@
+package npl;
+
+import jason.asSyntax.Literal;
+import jason.asSyntax.LogicalFormula;
+
+public abstract class AbstractNorm implements INorm {
+
+	protected String id;
+	protected LogicalFormula condition;
+	protected Literal consequence;
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public LogicalFormula getCondition() {
+		return condition;
+	}
+
+	@Override
+	public Literal getConsequence() {
+		return consequence;
+	}
+}
