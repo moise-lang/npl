66b65dee6c099d7343532219e0279b3d6e96b8c3
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-04-26 13:12:52 -0300

    Revert "Remove NormFactory"
    This reverts commit 221d013d7da5bac249e271dd278f4d73fbf9217f.

--- src/main/java/npl/Norm.java
+++ src/main/java/npl/Norm.java
@@ -12,20 +12,38 @@ public class Norm extends AbstractNorm {
      * argument.
      * 
      * @param id
-     *            Norm's id
+     *            norm's id
      * @param consequence
-     *            Norm's consequence
+     *            norm's consequence
      * @param condition
-     *            Norm's activation condition
+     *            norm's activation condition
+     */
+    public static NormFactory getFactory() {
+        return new NormFactory() {
+            public Norm createNorm(String id, Literal consequence, LogicalFormula activationCondition) {
+                boolean consequenceIsFailure = consequence.getFunctor().equals(NormativeProgram.FailFunctor);
+                if (!consequenceIsFailure) {
+                    String maintenanceConditionFunctor = ((Literal) consequence.getTerm(1)).getFunctor();
+                    if (maintenanceConditionFunctor.equals(id)) {
+                        consequence.setTerm(1, activationCondition);
+                    }
+                }
+                return new Norm(id, consequence, activationCondition);
+            }
+        };
+    }
+
+    /**
+     * Creates a norm based on the arguments' value without any modification.
+     * 
+     * @param id
+     *            norm's id
+     * @param consequence
+     *            norm's consequence
+     * @param condition
+     *            norm's activation condition
      */
     public Norm(String id, Literal consequence, LogicalFormula condition) {
-        boolean consequenceIsFailure = consequence.getFunctor().equals(NormativeProgram.FailFunctor);
-        if (!consequenceIsFailure) {
-            String maintenanceConditionFunctor = ((Literal) consequence.getTerm(1)).getFunctor();
-            if (maintenanceConditionFunctor.equals(id)) {
-                consequence.setTerm(1, condition);
-            }
-        }
         this.id = id;
         this.consequence = consequence;
         this.condition = condition;
@@ -34,10 +52,5 @@ public class Norm extends AbstractNorm {
     @Override
     public Norm clone() {
         return new Norm(id, consequence.copy(), (LogicalFormula) condition.clone());
-    }
-
-    @Override
-    public String toString() {
-        return "norm " + id + ": " + condition + " -> " + consequence;
     }
 }
---
+++ src/main/java/npl/NormFactory.java
@@ -0,0 +1,8 @@
+package npl;
+
+import jason.asSyntax.Literal;
+import jason.asSyntax.LogicalFormula;
+
+public interface NormFactory {
+    public Norm createNorm(String id, Literal consequence, LogicalFormula activationCondition);
+}
